54a41a694786eaedecc43259bafe571a7af77083
wdxxl <wdxxlanswer@gmail.com>
2016-03-21 04:20:23 -0700

    add local artifactory repo.

---
+++ publishToArtifactory/src/main/java/com/wdxxl/common/publishTask/SayHello2.java
@@ -0,0 +1,13 @@
+package com.wdxxl.common.publishTask;
+
+public class SayHello2 {
+
+	public void printHello2(String parameter) {
+		System.out.println("printHello :" + parameter);
+	}
+
+	public static void main(String[] args) {
+
+	}
+
+}
